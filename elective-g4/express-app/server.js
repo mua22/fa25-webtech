92d425c5aab52d3972f53ed0a36519cf879617c9
Usman Akram <usman.akram@gmail.com>
2025-11-13 15:28:19 +0500

    ejs for g4

--- elective-g4/express-app/server.js
+++ elective-g4/express-app/server.js
@@ -19,20 +19,27 @@ mongoose.connection.on("error", (err) => {
 mongoose.connection.on("disconnected", () => {
   console.log("Disconnected from MongoDB");
 });
-
+app.set("view engine", "ejs");
+app.use(express.json());
 const ProductModel = require("./models/product.model");
-app.post("/api/products", (req, res) => {
-  return res.send([]);
+
+app.post("/api/products", async (req, res) => {
+  let product = new ProductModel(req.body);
+  await product.save();
+  return res.send(product);
 });
-app.get("/api/products", (req, res) => {
-  return res.send(["Hello"]);
+
+app.get("/api/products", async (req, res) => {
+  let products = await ProductModel.find();
+  return res.send(products);
 });
 
 app.get("/", async (req, res) => {
   // return res.send("Hello World");
   let products = await ProductModel.find();
-  console.log(products);
-  return res.send(products);
+  // console.log(products);
+  // return res.send(products);
+  return res.render("homepage", { total: 5, products });
 });
 
 app.listen(3000, () => {
